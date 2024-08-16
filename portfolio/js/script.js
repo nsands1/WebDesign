401e40603a460797dc44bc04f267b82a1b2713f8
Neil <96086377+nsands1@users.noreply.github.com>
2024-08-15 17:23:17 -0700

    Color Adjustment and Animations

---
+++ portfolio/js/script.js
@@ -0,0 +1,7 @@
+/* ===== ===== ===== ===== ===== typing animation ===== ===== ===== ===== =====  */
+var typed = new Typed(".typing", {
+    strings:["", "Software Engineer","Web Designer","Web Developer","Video Game Creator","","US Veteran","Family Man",""],
+        typeSpeed: 100,
+        BackSpeed: 60,
+        loop: true
+})
